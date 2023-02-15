1a357afc4a3e5f75bfd6a3070bf00e46e8060ac0
Chintan Shingala <chintan.shingala@beatoapp.com>
2023-02-15 23:12:50 +0530

    @cs login signup fotter changes

--- src/RouteComponent.jsx
+++ src/RouteComponent.jsx
@@ -1,40 +1,24 @@
-import React from 'react'
-import {Routes,Route} from 'react-router-dom';
-import PageNotFound from './CommonComponent/Pagenotfound'
-import loader from "./assets/image/react.svg"
-const Home = React.lazy(()=>import('./Pages/Home'))
-const About = React.lazy(()=>import('./Pages/About'))
-const Learn = React.lazy(()=>import('./Pages/Learn'))
-const Login = React.lazy(()=>import('./Pages/Login'))
-const Register = React.lazy(()=>import('./Pages/Register'))
-// import DetailCategory from './CommonComponent/DetailCategory';
-// import Signup from './Pages/Signup';
-// import Signin from './Pages/Signin';const LazyLoading = React.lazy(()=>import('./Loading'))
-// const Shop = React.lazy(()=>import('./Pages/Shop'))
-// const Contact = React.lazy(()=>import('./Pages/Contact'))
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import PageNotFound from "./CommonComponent/Pagenotfound";
+import loader from "./assets/image/react.svg";
+import Home from "./Pages/Home";
+import About from "./Pages/About";
+import Learn from "./Pages/Learn";
+import Login from "./Pages/Login";
+import Register from "./Pages/Register";
 
 const RouteComponent = () => {
-    return (
-        <Routes>
-            <Route path="/" element={<React.Suspense fallback={<img src={loader}/>}><Home/></React.Suspense>} />
-            <Route path="/about" element={<React.Suspense fallback={<img src={loader}/>}><About/></React.Suspense>} />
-            <Route path="/learn" element={<React.Suspense fallback={<img src={loader}/>}><Learn/></React.Suspense>} />
-            <Route path="/login" element={<React.Suspense fallback={<img src={loader}/>}><Login/></React.Suspense>} />
-            <Route path="/register" element={<React.Suspense fallback={<img src={loader}/>}><Register/></React.Suspense>} />
-            <Route path="*" element={<React.Suspense fallback={<img src={loader}/>}><PageNotFound/></React.Suspense>} />
-            {/* <Route path="shop" element={<React.Suspense fallback={<img src={loader}/>}><Shop/></React.Suspense>} />
-            <Route path="contact" element={<React.Suspense fallback={<img src={loader}/>}><Contact/></React.Suspense>} />
-            <Route path="signup" element={<React.Suspense fallback={<img src={loader}/>}><Signup/></React.Suspense>} />
-            <Route path="signin" element={<React.Suspense fallback={<img src={loader}/>}><Signin/></React.Suspense>} /> */}
-            {/* <Route path="order-summery" element={<OrderSummery/>} />
-            <Route path="products" element={<Products/>}>
-            <Route index element={<FeatureProduct/>}/>
-                <Route path='featureproduct' element={<FeatureProduct/>}/>
-                <Route path='newproduct' element={<NewProduct/>}/>
-            </Route>*/}
-            {/* <Route path='category/:category' element={<DetailCategory/>}/> */}
-        </Routes>
-    )
-}
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/learn" element={<Learn />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="*" element={<PageNotFound />} />
+    </Routes>
+  );
+};
 
-export default RouteComponent
+export default RouteComponent;
